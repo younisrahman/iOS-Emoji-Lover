0fe6eac78ce010f2bd57ee20bd10aec2853a948c
Younis Rahman <younisrahman007@gmail.com>
2023-05-20 03:38:04 +0600

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Younis Rahman on 20/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
