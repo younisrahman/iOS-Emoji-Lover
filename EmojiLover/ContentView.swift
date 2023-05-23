99a2fc66d15b0caf9b09a291981512d43a2bfa14
Younis Rahman <younisrahman007@gmail.com>
2023-05-24 01:03:27 +0600

    change

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,26 @@
 
 import SwiftUI
 
+enum Emoji:String, CaseIterable {
+    case 🥳;
+    case 🦄;
+    case 🐉;
+    case 🍀;
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .🥳
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                Text(selection.rawValue).font(.system(size:200))
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id:\.self){emoji in Text(emoji.rawValue)}
+                }.pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lover").padding()
         }
-        .padding()
+       
     }
 }
 
@@ -24,3 +35,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
